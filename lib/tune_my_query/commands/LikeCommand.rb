5e3c21ed004aeba008e3a01ae9d822b5d3dd7ca7
Anuj Dutta <anuj@andhapp.com>
2011-01-16 23:44:56 +0000

    Fixed spaces.

--- lib/tune_my_query/commands/LikeCommand.rb
+++ lib/tune_my_query/commands/LikeCommand.rb
@@ -8,8 +8,8 @@ class LikeCommand
       if condition.is_a?(Array)
         condition.each { |c| execute(c) } # Calling itself on each element if the supplied parameter is an Array
       elsif condition.is_a?(String)
-        condition.gsub!(/ ilike /i, " like ") if condition.is_a?(String)          
-      end      
-    end    
+        condition.gsub!(/ ilike /i, " like ") if condition.is_a?(String)
+      end
+    end
   end
 end
--- lib/tune_my_query/tune_my_query.rb
+++ lib/tune_my_query/tune_my_query.rb
@@ -1,5 +1,5 @@
 module TuneMyQuery
-  
+
   # Hook invoked in include
   def self.included(base)
     base.class_eval do
@@ -10,42 +10,42 @@ module TuneMyQuery
       end
     end
   end
-  
+
   #                                                                                                   #
   # Adds tune_my_query method to ActiveRecord and its subclasses.                                     #
   #                                                                                                   #
-  # USAGE:                                                                                            #    
+  # USAGE:                                                                                            #
   #      tune_my_query :like                                                                          #
   #                                                                                                   #
   # 'like' signifies the command to be invoked.                                                       #
   # It also makes it apparent that an extension to LIKE needs to be standardise. Works only for       #
   # PostgreSQL specfific extensions. If MySql were to add its own extension to the standard SQL then  #
-  # this gem would cease to work.                                                                     #  
+  # this gem would cease to work.                                                                     #
   #                                                                                                   #
-  # METHODS:                                                                                          # 
+  # METHODS:                                                                                          #
   #                                                                                                   #
   # tune_my_query - Adds the command_type to to an instance variable namely, @command_type            #
   #                                                                                                   #
   # standardise_sql - Checks if PostgreSQLAdapter is used only then runs the conditions through the   #
   #                   Command. if not leaves the conditions untouched                                 #
   #                                                                                                   #
-  #                                                                                                   #  
-  module ClassMethods    
+  #                                                                                                   #
+  module ClassMethods
     def tune_my_query(command_type)
-       @command_type = command_type      
+       @command_type = command_type
     end
-    
+
     def standardise_sql(*conditions)
       if @command_type
         conditions = conditions.dup
         @adapter = connection && !connection.class.to_s.include?("PostgreSQLAdapter")
         command = "#{@command_type.to_s.camelize}Command".constantize
-        if @adapter      
+        if @adapter
           conditions.each { |condition| command.execute(condition) }
         end
       end
       super_merge_conditions(*conditions)
-    end    
+    end
   end
-  
+
 end
